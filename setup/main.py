92f1a8ce34080ae6f5ce51054bdef707c66df559
Mike FABIAN <mfabian@redhat.com>
2012-10-26 09:09:46 +0200

    Make applying the “Automatic reordering” setup option work

--- setup/main.py
+++ setup/main.py
@@ -127,6 +127,9 @@ class Setup ():
         word_commit = self.__word_commit.get_active()
         self.__write("WordCommit", word_commit)
 
+        auto_reorder = self.__auto_reorder.get_active()
+        self.__write("AutoReorder", auto_reorder)
+
         model = self.__hanja_key_list.get_model()
         str = ""
         iter = model.get_iter_first()
